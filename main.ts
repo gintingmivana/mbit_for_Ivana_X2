5ca5320d5511af03d23a57b900f344aa0c58272c
gintingmivana <gintingmivana@gmail.com>
2026-02-26 11:58:35 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,43 @@
+let X = [
+0,
+1,
+2,
+3,
+4,
+4,
+4,
+4,
+4,
+3,
+2,
+1,
+0,
+0,
+0,
+0
+]
+let y = [
+0,
+0,
+0,
+0,
+0,
+1,
+2,
+3,
+4,
+4,
+4,
+4,
+4,
+3,
+2,
+1
+]
+basic.forever(function () {
+    for (let index = 0; index <= X.length; index++) {
+        led.plot(X[index], y[index])
+        basic.pause(100)
+        basic.clearScreen()
+    }
+})
